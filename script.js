21c9cc99a940b1b35a70564b53beac6761579fa3
gaurav234 <91665015+gauravjoshi34@users.noreply.github.com>
2025-08-30 18:47:38 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,6 +2,25 @@ function mincost(arr)
 { 
 //write your code here
 // return the min cost
+	if(arr.length<1){
+		return 0;
+	}
+
+	let totalcost = 0;
+	arr.sort((a,b)=>a-b);
+	while(arr.length<1){
+		let firstrope = arr.shift();
+		let secondrope = arr.shift();
+
+		let newrope = firstrope + secondrope;
+
+		totalcost+=newrope;
+
+		arr.push(newrope);
+		arr.sort((a,b)=>a-b);
+	}
+
+	return totalcost;
   
 }
 
